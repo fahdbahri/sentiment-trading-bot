55ac8e243ceeda609f6f27fd27acc5d96d242c88
fahdbahri <fahdkhalid2002@gmail.com>
2025-03-24 10:05:56 +0200

    feat: add the price and the symbol

--- src/services/cryptonews.py
+++ src/services/cryptonews.py
@@ -1,4 +1,5 @@
 from pygooglenews import GoogleNews
+import requests
 
 
 gn = GoogleNews()
@@ -14,11 +15,67 @@ CRYPTOS = [
 
 def list_sources():
 
-    titles = []
+    crypto_data = []
+    
+
+    
+
+    
     for crypto in CRYPTOS:
+        price, symbol = get_price(crypto)
         response = gn.search(crypto, when='12h')
         for r in response['entries']:
-            titles.append(r['title'])
-    return titles
+            
+            titles = {
+                    "title": r['title'],
+                    "price": price,
+                    "symbol": symbol,
 
+                    }
+            crypto_data.append(titles)
+
+
+
+    print(crypto_data)
+
+
+
+
+def get_price(crypto):
+
+    if crypto == "bitcoin":
+        
+        url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+
+        response = requests.get(url).json()
+
+        return response['price'], response['symbol']
+
+    elif crypto == "ethereum":
+        
+        url = "https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT"
+
+        response = requests.get(url).json()
+
+        return response['price'], response['symbol']
+    elif crypto == "litecoin":
+        
+        url = "https://api.binance.com/api/v3/ticker/price?symbol=LTCUSDT"
+
+        response = requests.get(url).json()
+
+        return response['price'], response['symbol']
+        
+    elif crypto == "ripple":
+        
+        url = "https://api.binance.com/api/v3/ticker/price?symbol=XRPUSDT"
+
+        response = requests.get(url).json()
+
+        return response['price'], response['symbol']
+        
+
+
+      
+    
 
