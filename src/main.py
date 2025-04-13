76a8ff3f4ed0bd6ab3d0dde7046f66264016795d
fahdbahri <fahdkhalid2002@gmail.com>
2025-04-13 17:29:35 +0300

    fix: print statement and checking logs while runing the script in the background

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 from services.cryptonews import *
 from services.bot_sentiment import *
 import schedule
+import time
 
 
 
 def main():
+    print("Running...")
     news_data = list_sources()
+    print("Data fetched")
     get_sentiment_score(news_data)
+    print("Data sent to database")
 
 
 
